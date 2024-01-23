955ab190edddbc4307070f9fc3a30dafcf317e71
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-01-23 09:56:29 -0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from helper.webui import WebUI
 import helper.reporting_teardown as RT
 
 
-# Close WebUI and move report to Reports folder after the test session is completed
+# Close WebUI and move Allure report to Reports folder after the test session is completed
 def pytest_sessionfinish(session, exitstatus):
     print("\nTotal time (in seconds) spent on hard delays using WebUI.delay(): '"+str(WebUI.total_time_waited())+"' seconds waited")
     RT.reporting_teardown(session)
