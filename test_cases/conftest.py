343f7327ea8625ba4d2911535538594e78d14456
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-01-26 11:55:09 -0500

    add session back

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -2,5 +2,5 @@ from helper.global_config import private_config
 from keywords.webui.common import Common
 
 
-def pytest_sessionstart():
+def pytest_sessionstart(session):
     Common.login_to_truenas(private_config['USERNAME'], private_config['PASSWORD'])
--- test_cases/webui/login/conftest.py
+++ test_cases/webui/login/conftest.py
@@ -4,7 +4,7 @@ from helper.global_config import private_config
 from keywords.webui.common import Common
 
 
-def pytest_sessionfinish():
+def pytest_sessionfinish(session):
     Common.login_to_truenas(private_config['USERNAME'], private_config['PASSWORD'])
 
 
