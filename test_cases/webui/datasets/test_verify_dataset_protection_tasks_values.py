1876a6f257e999489c0886142797bb1c6a24ecad
ericbsd <ericturgeon.bsd@gmail.com>
2024-02-07 15:56:14 -0400

    Adding Test_Verify_Dataset_Protection_Tasks_Values

--- test_cases/webui/datasets/test_verify_dataset_protection_tasks_values.py
+++ test_cases/webui/datasets/test_verify_dataset_protection_tasks_values.py
@@ -1,33 +1,37 @@
 import pytest
 from helper.data_config import get_data_list
+from helper.global_config import shared_config
 from keywords.api.post import API_POST
 from keywords.api.delete import API_DELETE
-from keywords.webui.common import Common
 from keywords.webui.datasets import Datasets
 from keywords.webui.navigation import Navigation
 
 
 @pytest.mark.parametrize('data', get_data_list('datasets')[5:6], scope='class')
-class Test_Dataset_Protection_Navigate_Add_Snapshot:
+class Test_Verify_Dataset_Protection_Tasks_Values:
+    """
+    The test class for verifying the dataset protection tasks values.
+    """
     @staticmethod
     @pytest.fixture(scope='class', autouse=True)
-    def create_dataset(data):
+    def create_dataset_and_navigate_to_datasets_page(data):
         """
         This fixture creates a dataset for the test cass.
         """
         assert API_POST.create_dataset(f'{data["pool"]}/{data["dataset"]}').status_code == 200
-
-    @staticmethod
-    def on_the_datasets_page_verify_the_snapshot_opens(data):
-        """
-        This test verify verity the snapshot opens on the datasets page.
-        """
         Navigation.navigate_to_datasets()
         Datasets.assert_datasets_page_header()
+
+    @staticmethod
+    @pytest.mark.parametrize('task', shared_config['DATASET_PROTECTION_TASKS'])
+    def verify_dataset_protection_tasks_and_value(data, task):
+        """
+        This test verifies the dataset protection tasks is visible and its value is not empty.
+        """
         Datasets.select_dataset(data["dataset"])
-        Datasets.click_create_snapshot_button()
-        assert Datasets.is_add_snapshot_right_panel_visible() is True
-        Common.close_right_panel()
+        assert Datasets.is_protection_task_visible(task) is True
+        # With a new dataset the value is 0.
+        assert Datasets.get_protection_task_value(task) == '0'
 
     @staticmethod
     @pytest.fixture(scope='class', autouse=True)
