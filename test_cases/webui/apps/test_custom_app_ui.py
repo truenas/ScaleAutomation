1dffcb921a5b4f80f6a5a43c5212504df9de33ce
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-08 10:14:41 -0500

    Fix custom apps pool not configured error

--- test_cases/webui/apps/test_custom_app_ui.py
+++ test_cases/webui/apps/test_custom_app_ui.py
@@ -1,5 +1,10 @@
 import pytest
+
+import xpaths.common_xpaths
+from helper.global_config import shared_config
+from helper.webui import WebUI
 from keywords.webui.apps import Apps
+from keywords.webui.common import Common as COM
 
 
 class Test_Custom_App_UI:
@@ -9,6 +14,12 @@ class Test_Custom_App_UI:
         """
         This test verifies the ui for custom app
         """
+        if COM.is_visible(xpaths.common_xpaths.any_text('Apps Service Not Configured')):
+            COM.click_button('apps-settings')
+            COM.click_button('choose-pool')
+            COM.select_option('pool', 'pool-tank')
+            COM.click_button('choose')
+            WebUI.wait_until_not_visible(xpaths.common_xpaths.any_header('Configuring...', 1), shared_config['LONG_WAIT'])
         Apps.click_discover_apps()
         Apps.click_custom_app()
         assert Apps.assert_custom_app_ui() is True
