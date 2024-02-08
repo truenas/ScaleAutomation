5264a874703048828557a0f06c10a3c4cb2238ee
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-08 10:01:00 -0500

    remove random

--- test_cases/webui/dashboard/conftest.py
+++ test_cases/webui/dashboard/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from helper.global_config import private_config
-from helper.webui import WebUI
+from keywords.webui.common import Common
+from keywords.webui.navigation import Navigation
 
 
 @pytest.fixture(scope='class', autouse=True)
 def navigate_to_dashboard():
     # Ensure we are on the dashboard.
-    ip = private_config['IP']
-    WebUI.get(f'http://{ip}/ui/dashboard')
+    Navigation.navigate_to_dashboard()
