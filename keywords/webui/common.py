4e8b2afd2a3b7d06784470b37d4d37b7e61de6c2
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-08 08:46:14 -0500

    cleanup

--- keywords/webui/common.py
+++ keywords/webui/common.py
@@ -740,7 +740,7 @@ class Common:
         Example:
             - Common.set_input_field('myInput', 'text')
             - Common.set_input_field('myInput', 'text', True)
-            - Common.set_input_field('myInput', 'text', , true)
+            - Common.set_input_field('myInput', 'text', '', True)
         """
         WebUI.wait_until_visible(xpaths.common_xpaths.input_field(name))
         WebUI.xpath(xpaths.common_xpaths.input_field(name)).clear()
