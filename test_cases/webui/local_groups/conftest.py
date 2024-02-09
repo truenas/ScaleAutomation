6ae8350cbe387f2b311b40649b2b58ab7d18f6c0
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-09 12:47:48 -0500

    update to master

--- test_cases/webui/local_groups/conftest.py
+++ test_cases/webui/local_groups/conftest.py
@@ -1,16 +1,13 @@
 import pytest
-
 from helper.data_config import get_data_list
 from keywords.webui.local_groups import Local_Groups as LG
 from keywords.webui.navigation import Navigation
-
-
 @pytest.fixture(scope='class', autouse=True)
 def navigate_to_():
     """
-    This method starts all tests to navigate to the Data Protection page
+    This method starts all tests to navigate to the Local Groups page
     """
-    # Ensure we are on the Data Protection page.
+    # Ensure we are on the Local Groups page.
     Navigation.navigate_to_local_groups()
 
 
@@ -23,3 +20,17 @@ def setup_class(groups):
     # Setup clean environment.
     LG.delete_group_by_api(groups['group-name'], groups['group-privileges'])
     LG.delete_group_by_api(groups['alt-group-name'], groups['group-privileges'])
+
+
+@pytest.fixture(scope='class', autouse=True)
+@pytest.mark.parametrize('groups', get_data_list('local_groups'), scope='class')
+def teardown_class(groups):
+    """
+    This method clears any test groups after test is run for a clean environment
+    """
+    yield
+    # Clean up environment.
+    Navigation.navigate_to_dashboard()
+    LG.delete_group_by_api(groups['group-name'], groups['group-privileges'])
+    LG.delete_group_by_api(groups['alt-group-name'], groups['group-privileges'])
+    
