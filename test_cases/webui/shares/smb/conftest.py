521765c27999d4971893c079be4bede2b9194dbd
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-09 09:42:32 -0500

    move tests, add print to conftest

--- test_cases/webui/shares/smb/conftest.py
+++ test_cases/webui/shares/smb/conftest.py
@@ -3,8 +3,11 @@ from keywords.webui.common import Common
 
 
 def pytest_sessionstart(session):
+    print('in smb conftest start before')
     Common.create_non_admin_user_by_api('smbuser', 'smbuser Full', 'testing', 'True')
-
+    print('in smb conftest start after')
 
 def pytest_sessionfinish(session, exitstatus):
+    print('in smb conftest finish before')
     Common.delete_user_by_api('smbuser')
+    print('in smb conftest finish after')
