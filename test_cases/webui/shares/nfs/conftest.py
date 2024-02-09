e7c0efa374cb07df1a49da07f6457c382cee0e98
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-09 09:56:17 -0500

    change nfs and smb conftest to fixture

--- test_cases/webui/shares/nfs/conftest.py
+++ test_cases/webui/shares/nfs/conftest.py
@@ -1,17 +1,20 @@
+import pytest
+
 from keywords.webui.common import Common
 from keywords.webui.common_shares import Common_Shares as COMSHARE
 from keywords.webui.navigation import Navigation as NAV
 
 
-def pytest_sessionstart(session):
-    print('in nfs conftest start before')
+@pytest.fixture(scope='class', autouse=True)
+def setup_class():
+    print('in nfs setup before')
     Common.create_non_admin_user_by_api('nfs_test_user', 'NFS Test User', 'testing', 'True')
-    print('in nfs conftest start after')
+    print('in nfs setup after')
 
 
 def pytest_sessionfinish(session, exitstatus):
-    print('in nfs conftest finish before')
+    print('in nfs finish before')
     COMSHARE.delete_all_shares_by_sharetype('nfs')
     Common.delete_user_by_api('nfs_test_user')
     NAV.navigate_to_dashboard()
-    print('in nfs conftest finish after')
+    print('in nfs finish after')
--- test_cases/webui/shares/smb/conftest.py
+++ test_cases/webui/shares/smb/conftest.py
@@ -1,11 +1,14 @@
+import pytest
+
 from helper.global_config import private_config
 from keywords.webui.common import Common
 
-
-def pytest_sessionstart(session):
+@pytest.fixture(scope='class', autouse=True)
+def setup_class():
     print('in smb conftest start before')
     Common.create_non_admin_user_by_api('smbuser', 'smbuser Full', 'testing', 'True')
     print('in smb conftest start after')
+
 
 def pytest_sessionfinish(session, exitstatus):
     print('in smb conftest finish before')
