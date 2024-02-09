5064d8ca8d3ffea790afd4949a5d4a6e937d5cc4
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-09 09:48:10 -0500

    Update conftest.py

--- test_cases/webui/shares/nfs/conftest.py
+++ test_cases/webui/shares/nfs/conftest.py
@@ -4,10 +4,14 @@ from keywords.webui.navigation import Navigation as NAV
 
 
 def pytest_sessionstart(session):
+    print('in nfs conftest start before')
     Common.create_non_admin_user_by_api('nfs_test_user', 'NFS Test User', 'testing', 'True')
+    print('in nfs conftest start after')
 
 
 def pytest_sessionfinish(session, exitstatus):
+    print('in nfs conftest finish before')
     COMSHARE.delete_all_shares_by_sharetype('nfs')
     Common.delete_user_by_api('nfs_test_user')
     NAV.navigate_to_dashboard()
+    print('in nfs conftest finish after')
