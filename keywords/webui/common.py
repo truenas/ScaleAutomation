ba0877cdb19c1127b6943941b5eadc812eeac3e4
markbainix <mark.bain@ixsystems.com>
2024-02-16 14:21:02 -0500

    fixing missing row parameter

--- keywords/webui/common.py
+++ keywords/webui/common.py
@@ -814,9 +814,9 @@ class Common:
             - Common.set_checkbox_by_row_and_state('enabled', 1, True)
         """
         assert WebUI.wait_until_visible(xpaths.common_xpaths.checkbox_field_by_row(name, row)) is True
-        if WebUI.xpath(xpaths.common_xpaths.checkbox_field_by_row_attribute(name)).get_property('checked') is not state:
+        if WebUI.xpath(xpaths.common_xpaths.checkbox_field_by_row_attribute(name, row)).get_property('checked') is not state:
             WebUI.xpath(xpaths.common_xpaths.checkbox_field_by_row(name, row)).click()
-        assert WebUI.xpath(xpaths.common_xpaths.checkbox_field_by_row_attribute(name)).get_property('checked') is state
+        assert WebUI.xpath(xpaths.common_xpaths.checkbox_field_by_row_attribute(name, row)).get_property('checked') is state
 
     @classmethod
     def set_checkbox_by_state(cls, name: str, state: bool) -> None:
