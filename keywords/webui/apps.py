334aca887a282867a3b6e8fd5ebc265d17f0a809
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-20 08:06:24 -0500

    Update apps.py

--- keywords/webui/apps.py
+++ keywords/webui/apps.py
@@ -456,10 +456,7 @@ class Apps:
             Apps.click_install_app(name)
             Apps.set_app_values(name)
             COM.click_save_button()
-            if WebUI.wait_until_visible(xpaths.common_xpaths.any_header('Docker Hub Rate Limit Warning', 1), shared_config['SHORT_WAIT']):
-                COM.assert_confirm_dialog()
-                if COM.is_clickable(xpaths.common_xpaths.button_field('save')):
-                    COM.click_save_button()
+            cls.handle_docker_limit_dialog()
             assert COM.assert_page_header('Installed', shared_config['LONG_WAIT'])
         assert Apps.is_app_installed(name) is True
         return Apps.is_app_deployed(name) is True
