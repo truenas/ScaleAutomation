e87aede67e0ecc3cd60fa31d46bb609d82903754
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-22 15:57:00 -0500

    Create system_services_edit.py

---
+++ keywords/webui/system_services_edit.py
@@ -0,0 +1,31 @@
+import xpaths
+from helper.webui import WebUI
+from keywords.api.post import API_POST
+from keywords.api.put import API_PUT
+from keywords.webui.common import Common as COM
+from keywords.webui.system_services import System_Services as SERV
+
+
+services_list = ['ftp', 'iscsi', 'nfs', 'smart', 'smb', 'snmp', 'ssh', 'ups']
+ftp_fields = ['General Options', 'port', 'clients', 'ipconnections', 'loginattempt', 'timeout-notransfer', 'timeout']
+iscsi_fields = ['Global Configuration', 'basename', 'isns-servers', 'pool-avail-threshold', 'listen-port']
+nfs_fields = ['General Options', 'bindip', 'servers', 'NFSv4', 'protocols', 'v-4-v-3-owner', 'v-4-krb', 'Ports', 'mountd-port', 'rpcstatd-port', 'rpclockd-port', 'Other Options', 'udp', 'allow-nonroot', 'userd-manage-gids']
+smart_fields =['General Options', 'interval', 'powermode', 'difference', 'informational', 'critical']
+smb_fields = ['NetBIOS', 'netbiosname', 'netbiosalias', 'workgroup', 'description', 'enable-smb-1', 'ntlmv-1-auth']
+snmp_fields = ['General Options', 'location', 'contact', 'community', 'Other Options', 'options', 'zilstat', 'loglevel', 'SNMP v3 Options', 'v-3']
+ssh_fields = ['General Options', 'tcpport', 'password-login-groups', 'passwordauth', 'kerberosauth', 'tcpfwd']
+ups_fields = ['General Options', 'identifier', 'mode', 'driver', 'port', 'Shutdown', 'shutdown', 'shutdowntimer', 'shutdowncmd', 'powerdown', 'Other Options', 'nocommwarntime', 'hostsync', 'description', 'options', 'optionsupsd', 'Monitor', 'monuser', 'monpwd', 'extrausers', 'rmonitor']
+
+
+class system_service_edit:
+    @classmethod
+    def click_Service_Name_Edit_button(cls, servicename: str):
+        name = SERV.return_backend_service_name(servicename)
+        COM.click_button(f'{name}-edit')
+        WebUI.wait_until_visible(xpaths.common_xpaths.any_header(servicename, 3))
+
+    @classmethod
+    def is_edit_service_ui_tabs(cls, servicename: str) -> bool:
+        name = SERV.return_backend_service_name(servicename)
+        return COM.is_visible(xpaths.common_xpaths.button_field(f'{name}-edit'))
+
