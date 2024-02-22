9be851c9a37ffafb5745025ccfbb076abfe44987
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-02-22 09:39:23 -0500

    fix datasets being left behind

--- test_cases/webui/datasets/test_create_dataset.py
+++ test_cases/webui/datasets/test_create_dataset.py
@@ -5,11 +5,28 @@ from keywords.webui.common import Common
 from keywords.webui.datasets import Datasets
 from keywords.webui.navigation import Navigation
 
+
 @pytest.mark.parametrize('data', get_data_list('datasets')[2:4], scope='class')
 class Test_Create_Dataset:
     """
     This test cass creates a dataset and verifies it is in the datasets page.
     """
+
+    @pytest.fixture(scope='function', autouse=True)
+    def setup_class(self, data):
+        """
+        This fixture deletes the created dataset for the class.
+        """
+        API_DELETE.delete_dataset(f'{data["pool"]}/{data["dataset"]}')
+
+    @pytest.fixture(scope='function', autouse=True)
+    def teardown_class(self, data):
+        """
+        This fixture deletes the created dataset for the class.
+        """
+        yield
+        API_DELETE.delete_dataset(f'{data["pool"]}/{data["dataset"]}')
+
     def test_create_dataset(seff, data):
         """
         This test navigates to datasets page.
@@ -25,16 +42,7 @@ class Test_Create_Dataset:
         # Create a dataset and save it.
         Common.assert_right_panel_header('Add Dataset')
         Datasets.set_dataset_name(data["dataset"])
-        Common.click_save_button()
-        Common.assert_progress_bar_not_visible()
+        Common.click_save_button_and_wait_for_progress_bar()
 
         # Verify that the created dataset exists in the datasets page.
         Datasets.is_dataset_visible(data["pool"], data["dataset"])
-
-    @pytest.fixture(scope='class', autouse=True)
-    def tear_down_class(self, data):
-        """
-        This fixture deletes the created dataset for the class.
-        """
-        yield
-        API_DELETE.delete_dataset(f'{data["pool"]}/{data["dataset"]}')
