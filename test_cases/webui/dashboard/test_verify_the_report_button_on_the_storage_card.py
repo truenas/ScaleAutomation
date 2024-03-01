7533b05556a6d7f62e84d86fe5798e88ad0f93f7
ericbsd <ericturgeon.bsd@gmail.com>
2024-03-01 11:31:25 -0400

    Simplified Test_Verify_The_Report_Button_On_The_Storage_Card

--- test_cases/webui/dashboard/test_verify_the_report_button_on_the_storage_card.py
+++ test_cases/webui/dashboard/test_verify_the_report_button_on_the_storage_card.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from time import sleep
 from helper.global_config import shared_config
@@ -5,21 +6,15 @@ from keywords.webui.dashboard import Dashboard
 from keywords.webui.reporting import Reporting
 
 
+@allure.epic('Dashboard')
+@allure.feature('Storage Card')
 class Test_Verify_The_Report_Button_On_The_Storage_Card:
-
-    @staticmethod
-    def on_the_dashboard_click_on_the_storage_report_button():
+    @allure.story('The Report Button On The Storage Card Works')
+    def test_verifying_the_report_button_on_the_storage_card_works(self):
         assert Dashboard.assert_dashboard_page_header_is_visible() is True
         assert Dashboard.is_storage_card_visible() is True
         Dashboard.click_the_storage_report_button()
-
-    @staticmethod
-    def verify_storage_reporting_page_open():
+        # Verify storage reporting page open
         assert Reporting.assert_reporting_page_breadcrumb() is True
         assert Reporting.assert_storage_reporting_page_header() is True
-        sleep(5)
-
-    @staticmethod
-    @pytest.mark.parametrize('disk', shared_config['DISK_LIST'])
-    def verify_disk_exist_card(disk):
-        assert Reporting.is_disk_i_o_card_visible(disk) is True
+        assert Reporting.is_disk_i_o_card_visible('sd') is True
