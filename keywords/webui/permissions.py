0fc82a7f0e5b9f8656d55fef955e8b051979605f
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-03-04 13:56:14 -0500

    add docstrings to methods

--- keywords/webui/permissions.py
+++ keywords/webui/permissions.py
@@ -6,10 +6,11 @@ class Permissions:
     @classmethod
     def get_dataset_permissions_by_level(cls, user_category: str, level: str) -> str:
         """
+        This method returns the permissions text for the given user category for the given level.
 
-        :param user_category:
-        :param level:
-        :return:
+        :param user_category: the user type. EX: person (user/owner), people (group), groups (other).
+        :param level: the permissions level. EX: read, write, execute.
+        :return: returns the permissions text for the given user category for the given level.
         """
         return COM.get_element_property(f"//*[@name='{user_category}']/ancestor::ix-permissions-item/descendant::*[@class='{level}']", "textContent")
 
@@ -84,18 +85,20 @@ class Permissions:
     @classmethod
     def verify_dataset_group(cls, name: str) -> bool:
         """
+        This method returns true if the textContent of the group element matches the given name.
 
-        :param name:
-        :return:
+        :param name: The name of the group.
+        :return: returns true if the textContent of the element matches the given name.
         """
         return COM.get_element_property(xpaths.common_xpaths.any_xpath(f"//*[contains(text(),'Group:')]/..//*[contains(text(),'{name}')]"), 'textContent')
 
     @classmethod
     def verify_dataset_group_permissions(cls, permissions: str) -> bool:
         """
+        This method returns true if the group permissions visible matches the given permissions.
 
-        :param permissions:
-        :return:
+        :param permissions: the expected permissions of the group.
+        :return: returns true if the group permissions visible match the given permissions.
         """
         val = cls.get_dataset_permissions_by_level('people', 'permissions')
         return val == permissions
@@ -103,9 +106,10 @@ class Permissions:
     @classmethod
     def verify_dataset_group_permissions_name(cls, name: str) -> bool:
         """
+        This method returns true if the name of the group visible matches the given name.
 
-        :param name:
-        :return:
+        :param name: the name of the group.
+        :return: returns true if the name of the group visible matches the given name.
         """
         val = cls.get_dataset_permissions_by_level('people', 'name')
         return val == name
@@ -113,9 +117,10 @@ class Permissions:
     @classmethod
     def verify_dataset_other_permissions(cls, permissions: str) -> bool:
         """
+        This method returns true if the other permissions visible matches the given permissions.
 
-        :param permissions:
-        :return:
+        :param permissions: the expected permissions of other.
+        :return: returns true if the other permissions visible match the given permissions.
         """
         val = cls.get_dataset_permissions_by_level('groups', 'permissions')
         return val == permissions
@@ -123,8 +128,9 @@ class Permissions:
     @classmethod
     def verify_dataset_other_permissions_name(cls) -> bool:
         """
+        This method returns true if the name other is visible for the other permissions.
 
-        :return:
+        :return: returns true if the name other is visible for the other permissions.
         """
         val = cls.get_dataset_permissions_by_level('groups', 'name')
         return val == 'Other'
@@ -132,18 +138,20 @@ class Permissions:
     @classmethod
     def verify_dataset_owner(cls, name: str) -> bool:
         """
+        This method returns true if the textContent of the owner element matches the given name.
 
-        :param name:
-        :return:
+        :param name: The name of the owner.
+        :return: returns true if the textContent of the element matches the given name.
         """
         return COM.get_element_property(xpaths.common_xpaths.any_xpath(f"//*[contains(text(),'Owner:')]/..//*[contains(text(),'{name}')]"), 'textContent')
 
     @classmethod
     def verify_dataset_owner_permissions(cls, permissions: str) -> bool:
         """
+        This method returns true if the owner permissions visible matches the given permissions.
 
-        :param permissions:
-        :return:
+        :param permissions: the expected permissions of the owner.
+        :return: returns true if the owner permissions visible match the given permissions.
         """
         val = cls.get_dataset_permissions_by_level('person', 'permissions')
         return val == permissions
@@ -151,9 +159,10 @@ class Permissions:
     @classmethod
     def verify_dataset_owner_permissions_name(cls, name: str) -> bool:
         """
+        This method returns true if the name of the owner visible matches the given name.
 
-        :param name:
-        :return:
+        :param name: the name of the owner.
+        :return: returns true if the name of the owner visible matches the given name.
         """
         val = cls.get_dataset_permissions_by_level('person', 'name')
         return val == name
@@ -161,17 +170,19 @@ class Permissions:
     @classmethod
     def verify_dataset_permissions_edit_button(cls) -> bool:
         """
+        This method returns true if the edit permissions button is visible.
 
-        :return:
+        :return: returns true if the edit permissions button is visible.
         """
         return COM.is_visible(xpaths.common_xpaths.link_field('edit-permissions'))
 
     @classmethod
-    def verify_dataset_permissions_type(cls, permtype: str) -> bool:
+    def verify_dataset_permissions_type(cls, permissions_type: str) -> bool:
         """
+        This method returns true if the permissions type is visible.
 
-        :param permtype:
-        :return:
+        :param permissions_type: the type of permission level for the dataset.
+        :return: returns true if the permissions type is visible.
         """
-        return COM.assert_text_is_visible(permtype)
+        return COM.assert_text_is_visible(permissions_type)
 
