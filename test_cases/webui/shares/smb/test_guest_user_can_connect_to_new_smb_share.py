0285299947ce277887a8b963ef426bdfabf90685
markbainix <mark.bain@ixsystems.com>
2024-03-12 10:30:03 -0400

    clean up

--- test_cases/webui/shares/smb/test_guest_user_can_connect_to_new_smb_share.py
+++ test_cases/webui/shares/smb/test_guest_user_can_connect_to_new_smb_share.py
@@ -24,14 +24,6 @@ class Test_SMB:
         API_POST.create_dataset('tank/SMBGUEST', 'SMB')
         API_POST.create_share('smb', 'SMBGUEST', '/mnt/tank/SMBGUEST', True)
         API_POST.create_non_admin_user('smbguest', 'smbguest Full', 'testing', 'True')
-        NAV.navigate_to_shares()
-        SMB.click_edit_share_filesystem_acl('SMBGUEST')
-        PERM.select_ace_who('user')
-        PERM.select_ace_user('smbguest')
-        PERM.set_ace_permissions('FULL CONTROL')
-        PERM.click_save_acl_button()
-        SERVICE.start_service_by_api('cifs')
-        SERVICE.start_service_by_api('ssh')
 
     @pytest.fixture(scope='function', autouse=True)
     def teardown_test(self) -> None:
@@ -49,4 +41,12 @@ class Test_SMB:
     def test_guest_user_can_connect_to_new_smb_share(self) -> None:
 
         # Verify SMB Guest can access Share
+        NAV.navigate_to_shares()
+        SMB.click_edit_share_filesystem_acl('SMBGUEST')
+        PERM.select_ace_who('user')
+        PERM.select_ace_user('smbguest')
+        PERM.set_ace_permissions('FULL CONTROL')
+        PERM.click_save_acl_button()
+        SERVICE.start_service_by_api('cifs')
+        SERVICE.start_service_by_api('ssh')
         assert SMB.assert_guest_access('SMBGUEST','smbguest', 'testing') is True
