b83bba6a710b32793022b6fc573b120d4fe1d233
markbainix <mark.bain@ixsystems.com>
2024-03-15 14:27:29 -0400

    fix updating ACL dialog waits

--- keywords/webui/permissions.py
+++ keywords/webui/permissions.py
@@ -113,7 +113,8 @@ class Permissions:
             - Permissions.click_save_acl_button()
         """
         COM.click_button('save-acl')
-        COM.assert_dialog_not_visible('Updating ACL', shared_config['LONG_WAIT'])
+        assert COM.assert_dialog_visible('Updating ACL', shared_config['WAIT']) is True
+        assert COM.assert_dialog_not_visible('Updating ACL', shared_config['LONG_WAIT']) is True
 
     @classmethod
     def get_dataset_permissions_by_level(cls, user_category: str, level: str) -> str:
