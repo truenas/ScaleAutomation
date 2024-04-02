fda0defbb70a63c747366eecb25408b02100d2a9
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-04-02 16:00:54 -0400

    pivot test to IP instead of host

--- keywords/ssh/nfs.py
+++ keywords/ssh/nfs.py
@@ -31,4 +31,75 @@ class SSH_NFS:
         response = SSH_Command_Line('mount', private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
         return mount_path not in response.stdout
 
+    @classmethod
+    def verify_share_delete_access(cls, mount_path: str) -> bool:
+        """
+        This method attempts to access the given dataset with the given username and preform write actions.
 
+        :param mount_path: the path from 'nfsshares' to the directory to mount the share to.
+        :return: True if the dataset is accessible with write access, False otherwise.
+        """
+        file = f"test_delete_file.txt"
+        command = f"cd ~/nfsshares ; sudo touch {mount_path}/{file}"
+        SSH_Command_Line(command, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        command2 = f"cd ~/nfsshares ; sudo ls -al {mount_path}"
+        value = SSH_Command_Line(command2, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        assert file in value.stdout
+        command3 = f"cd ~/nfsshares ; rm -rf {mount_path}/{file}"
+        value2 = SSH_Command_Line(command3, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        return file not in value2.stdout.lower()
+
+    @classmethod
+    def verify_share_execute_access(cls, mount_path: str) -> bool:
+        """
+        This method attempts to access the given dataset with the given username and preform execute actions.
+
+        :param mount_path: the path from 'nfsshares' to the directory to mount the share to.
+        :return: True if the dataset is accessible with execute access, False otherwise.
+        """
+        # leaving commented for when we need to test file execution permissions.
+        file = f"test_exec_file.sh"
+        cr_dir = f"test_exec_dir_internal"
+        command = f'cd ~/nfsshares/test_execute_dir_external ; chmod 777 . ; touch {file} ;  echo -n "mkdir ~/nfsshares/{mount_path}/{cr_dir}" | cat > {file} ; chmod 777 {file}'
+        SSH_Command_Line(command, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        command2 = f"cd ~/nfsshares/test_execute_dir_external ; ./{file}"
+        SSH_Command_Line(command2, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        command3 = f"cd ~/nfsshares ; sudo ls -al {mount_path}"
+        value = SSH_Command_Line(command3, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        return cr_dir in value.stdout.lower()
+        # command = f"cd /mnt/{pool}/{dataset}"
+        # value = SSH_Command_Line(command, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        # if "permission denied" in value.stderr.lower():
+        #     print("Permission denied while running command.")
+        #     return False
+        # return True
+
+    @classmethod
+    def verify_share_read_access(cls, mount_path: str) -> bool:
+        """
+        This method verifies the share read access.
+
+        :param mount_path: the path from 'nfsshares' to the directory to mount the share to.
+        :return:
+        """
+        command = f"cd ~/nfsshares/{mount_path} ; ls -al"
+        value = SSH_Command_Line(command, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        if "permission denied" in value.stderr.lower():
+            print("Permission denied while running command.")
+            return False
+        return True
+
+    @classmethod
+    def verify_share_write_access(cls, mount_path: str) -> bool:
+        """
+        This method attempts to access the given dataset with the given username and preform write actions.
+
+        :param mount_path: the path from 'nfsshares' to the directory to mount the share to.
+        :return: True if the dataset is accessible with write access, False otherwise.
+        """
+        file = f"test_write_file.txt"
+        command = f"cd ~/nfsshares ; touch {mount_path}/{file}"
+        SSH_Command_Line(command, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        command2 = f"cd ~/nfsshares ; sudo ls -al {mount_path}"
+        value = SSH_Command_Line(command2, private_config['NFS_CLIENT_IP'], private_config['NFS_CLIENT_USERNAME'], private_config['NFS_CLIENT_PASSWORD'])
+        return file in value.stdout.lower()
--- test_cases/webui/shares/nfs/test_authorized_hosts.py
+++ test_cases/webui/shares/nfs/test_authorized_hosts.py
@@ -16,7 +16,7 @@ from keywords.ssh.nfs import SSH_NFS as NFS_SSH
 @allure.epic("Shares")
 @allure.feature("NFS")
 @pytest.mark.parametrize('nfs_data', get_data_list('shares/nfs'))
-class Test_Authorized_Hosts:
+class Test_Authorized_IP_Addresses:
     @pytest.fixture(scope='function', autouse=True)
     def setup_test(self, nfs_data):
         """
@@ -28,6 +28,10 @@ class Test_Authorized_Hosts:
         API_POST.start_service('nfs')
         API_POST.create_dataset(nfs_data['api_path'], 'NFS')
         API_POST.create_share('nfs', '', nfs_data['share_page_path'])
+
+        # TODO: So this needs to have other permissions write, alowed or it needs to have a UID matched user own it like nfs_user (3000)
+        # It appears to be working, just need to sort that out. Nobody nogroup is not needed for this test.
+        # API_POST.set_dataset_permissions_user_and_group(nfs_data['api_path'], 'nobody', 'nogroup')
 
         # Navigate to the Shares page
         NAV.navigate_to_shares()
@@ -44,13 +48,13 @@ class Test_Authorized_Hosts:
         API_DELETE.delete_dataset(nfs_data['api_path'], recursive=True, force=True)
         assert NFS_SSH.unmount_nfs_share('auth_host') is True
 
-    @allure.tag("Authorized Hosts")
-    @allure.story("Edit NFS Share Advanced Options")
-    def test_nfs_share_authorized_hosts(self, nfs_data):
+    @allure.tag("Authorized IP Addresses")
+    @allure.story("NFS Share Authorized IP Addresses")
+    def test_nfs_share_authorized_ip_addresses(self, nfs_data):
         """
         This test edits the NFS share with an authorized host and verifies that the share can/cannot be mounted.
         """
-        # Edit the NFS share and set a valid authorized host
+        # Edit the NFS share and set a valid authorized ip address
         assert COMSHARE.assert_share_path('nfs', nfs_data['share_page_path']) is True
         COMSHARE.click_edit_share('nfs', nfs_data['share_page_path'])
         COM.set_checkbox('enabled')
@@ -61,12 +65,20 @@ class Test_Authorized_Hosts:
         # Verify share can mount
         assert COMSHARE.is_share_enabled('nfs', nfs_data['share_page_path']) is True
         assert NFS_SSH.mount_nfs_share(nfs_data['share_page_path'], 'auth_host') is True
+        assert NFS_SSH.verify_share_read_access('auth_host') is True
+        assert NFS_SSH.verify_share_write_access('auth_host') is True
+        assert NFS_SSH.verify_share_execute_access('auth_host') is True
+        assert NFS_SSH.verify_share_delete_access('auth_host') is True
         assert NFS_SSH.unmount_nfs_share('auth_host') is True
-        # Edit the NFS share and set an invalid authorized host
+        # Edit the NFS share and set an invalid authorized ip address
         COMSHARE.click_edit_share('nfs', nfs_data['share_page_path'])
         NFS.set_host_and_ip(private_config['NFS_CLIENT_IP']+'1')
         COM.click_save_button_and_wait_for_right_panel()
         # Verify share cannot mount
         assert COMSHARE.is_share_enabled('nfs', nfs_data['share_page_path']) is True
         assert NFS_SSH.mount_nfs_share(nfs_data['share_page_path'], 'auth_host') is False
+        assert NFS_SSH.verify_share_read_access('auth_host') is False
+        assert NFS_SSH.verify_share_write_access('auth_host') is False
+        assert NFS_SSH.verify_share_execute_access('auth_host') is False
+        assert NFS_SSH.verify_share_delete_access('auth_host') is False
 
