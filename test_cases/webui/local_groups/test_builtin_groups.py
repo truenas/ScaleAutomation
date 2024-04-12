121f7e100fe2dbc5a22fe20a24043fd6954f635c
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-04-12 08:54:37 -0400

    docsting update
    (cherry picked from commit f4bb5e1edcc34907f96b81ac588de9fafa8801ab)

--- test_cases/webui/local_groups/test_builtin_groups.py
+++ test_cases/webui/local_groups/test_builtin_groups.py
@@ -14,7 +14,7 @@ class Test_Builtin_Groups:
     @pytest.fixture(scope='class', autouse=True)
     def setup_test(self) -> None:
         """
-        This method sets up each test to start with builtin groups not shown
+        This method navigates to Local Groups, sets the list to 100 items and enables built-in groups toggle.
         """
         NAV.navigate_to_local_groups()
         COM.set_100_items_per_page()
@@ -23,11 +23,12 @@ class Test_Builtin_Groups:
     @pytest.fixture(scope='class', autouse=True)
     def teardown_test(self):
         """
-        This method clears any test groups after test is run for a clean environment
+        This method unsets the show built-in groups toggle and navigates to dashboard
         """
         yield
         # Clean up environment.
         LG.unset_show_builtin_groups_toggle()
+        NAV.navigate_to_dashboard()
 
     @allure.tag("Read")
     @allure.story("Verify Built in Local Groups")
