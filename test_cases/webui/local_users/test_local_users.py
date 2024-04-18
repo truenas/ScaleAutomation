8d97e3b193bda9f227117ade94b737feb6c931ec
markbainix <mark.bain@ixsystems.com>
2024-04-18 14:12:04 -0400

    added test steps in doc block

--- test_cases/webui/local_users/test_local_users.py
+++ test_cases/webui/local_users/test_local_users.py
@@ -43,7 +43,13 @@ class Test_Local_Users:
     @allure.story("Add New Local Users")
     def test_add_new_user(self, users) -> None:
         """
-        This test verifies a new user can be created
+        Summary: This test verifies a new user can be created
+
+        Test Steps:
+        1. Click Add User button
+        2. Edit User values (username, fullname, password, confirm password, email)
+        3. Click Save button
+        4. Verify User is visible
         """
         API_DELETE.delete_user(users['username'])
         assert LU.is_user_visible(users['username']) is False
@@ -65,7 +71,12 @@ class Test_Local_Users:
     @allure.story("Delete Local Users")
     def test_delete_new_user(self, users) -> None:
         """
-        This test verifies a new user can be deleted
+        Summary: This test verifies a new user can be deleted
+
+        Test Steps:
+        1. Verify User is visible
+        2. Expand User then Delete User and confirm
+        3. Verify User is Not visible
         """
         assert LU.is_user_visible(users['username'])
 
@@ -78,7 +89,13 @@ class Test_Local_Users:
     @allure.story("Edit Local Users - Authentication Fields")
     def test_edit_user_authentication_fields(self, users) -> None:
         """
-        This test verifies a new user can be created
+        Summary: This test verifies user authentication fields can be edited
+
+        Test Steps:
+        1. Verify User is visible
+        2. Edit User Authentication fields
+        3. Click Save button
+        4. Verify User Authentication fields are set
         """
         assert LU.is_user_visible(users['username']) is True
 
@@ -139,7 +156,13 @@ class Test_Local_Users:
     @allure.story("Edit Local Users - Directory Permissions")
     def test_edit_user_directory_permissions(self, users) -> None:
         """
-        This test verifies the user directory permissions can be edited
+        Summary: This test verifies user directory permission fields can be edited
+
+        Test Steps:
+        1. Verify User is visible
+        2. Edit User directory permission fields
+        3. Click Save button
+        4. Verify User directory permission fields are set
         """
         assert LU.is_user_visible(users['username']) is True
 
@@ -198,7 +221,13 @@ class Test_Local_Users:
     @allure.story("Edit Local Users - Identification Fields")
     def test_edit_user_identification_fields(self, users) -> None:
         """
-        This test verifies the user identification fields can be edited
+        Summary: This test verifies user identification fields can be edited
+
+        Test Steps:
+        1. Verify User is visible
+        2. Edit User identification fields
+        3. Click Save button
+        4. Verify User identification fields are set
         """
         assert LU.is_user_visible(users['username']) is True
 
@@ -230,7 +259,13 @@ class Test_Local_Users:
     @allure.story("Edit Local Users - User ID and Group Fields")
     def test_edit_user_user_id_and_group_fields(self, users) -> None:
         """
-        This test verifies the user id and group fields can be edited
+        Summary: This test verifies user id and group fields can be edited
+
+        Test Steps:
+        1. Verify User is visible
+        2. Edit user id and group fields
+        3. Click Save button
+        4. Verify user id and group fields are set
         """
         assert LU.is_user_visible(users['username']) is True
 
@@ -254,7 +289,14 @@ class Test_Local_Users:
     @allure.story("Edit Local Users - Login with New User")
     def verify_user_login_after_edit(self, users) -> None:
         """
-        This test verifies the user can log in after identification fields edited
+        Summary: This test verifies user can log in after identification fields edited
+
+        Test Steps:
+        1. Verify User is visible
+        2. Edit User identification fields
+        3. Click Save button
+        4. Log off
+        4. Verify User can log in
         """
         assert LU.is_user_visible(users['username']) is True
 
