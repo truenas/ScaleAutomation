b536897217a7cc75e546135cb44455fdfaacf9e1
markbainix <mark.bain@ixsystems.com>
2024-04-18 15:00:49 -0400

    fixed built in user xpaths

--- xpaths/local_users.py
+++ xpaths/local_users.py
@@ -1,4 +1,44 @@
 
+def builtin(name: str) -> str:
+    """
+    This function sets the text for the given local username
+
+    :param name: text of the given username
+    :return: xpath string for given username
+    """
+    return f'//*[@data-test="text-builtin-user-{name}-row-yesno"]'
+
+
+def full_name(name: str) -> str:
+    """
+    This function sets the text for the given local username
+
+    :param name: text of the given username
+    :return: xpath string for given username
+    """
+    return f'//*[@data-test="text-full-name-user-{name}-row-text"]'
+
+
+def roles(name: str) -> str:
+    """
+    This function sets the text for the given local username
+
+    :param name: text of the given username
+    :return: xpath string for given username
+    """
+    return f'//*[@data-test="text-roles-user-{name}-row-text"]'
+
+
+def uid(name: str) -> str:
+    """
+    This function sets the text for the given local username
+
+    :param name: text of the given username
+    :return: xpath string for given username
+    """
+    return f'//*[@data-test="text-uid-user-{name}-row-text"]'
+
+
 def user(name: str) -> str:
     """
     This function sets the text for the given local username
