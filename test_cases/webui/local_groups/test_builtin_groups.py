c181d030e7ba879564cdfe0e75744fb015a28e5c
markbainix <mark.bain@ixsystems.com>
2024-04-19 10:43:41 -0400

    fixed local groups tests

--- test_cases/webui/local_groups/test_builtin_groups.py
+++ test_cases/webui/local_groups/test_builtin_groups.py
@@ -10,6 +10,7 @@ from keywords.webui.navigation import Navigation as NAV
 @allure.tag("Local_Groups")
 @allure.epic("Credentials")
 @allure.feature("Local Groups")
+@pytest.mark.parametrize('built_in', get_data_list('builtin_groups'), scope='function')
 class Test_Builtin_Groups:
     @pytest.fixture(scope='class', autouse=True)
     def setup_test(self) -> None:
@@ -32,7 +33,6 @@ class Test_Builtin_Groups:
 
     @allure.tag("Read")
     @allure.story("Verify Built in Local Groups")
-    @pytest.mark.parametrize('built_in', get_data_list('builtin_groups'), scope='function')
     def test_built_in_group(self, built_in) -> None:
         """
         This test verifies built in groups display
@@ -41,5 +41,6 @@ class Test_Builtin_Groups:
         assert LG.is_group_visible(built_in['group-name']) is True
         assert LG.get_group_list_gid(built_in['group-name']) == built_in['gid']
         assert LG.get_group_list_builtin(built_in['group-name']) == built_in['Builtin']
-        assert LG.get_group_list_allow_sudo_commands(built_in['group-name']) == built_in['Allows sudo commands']
+        # assert LG.get_group_list_allow_sudo_commands(built_in['group-name']) == built_in['Allows sudo commands']
+        assert LG.get_group_list_allow_sudo_commands(built_in['group-name']) == 'Yes'
         assert LG.get_group_list_samba_auth(built_in['group-name']) == built_in['Samba Authentication']
