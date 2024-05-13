f0410d3244e2923d57e92675a9515fc1353a36e3
jordan-mcgillivray005 <120522382+jordan-mcgillivray005@users.noreply.github.com>
2024-05-13 09:03:01 -0400

    Fix remove_ldap is visible skip, add NAS-128958

--- keywords/webui/directory_services.py
+++ keywords/webui/directory_services.py
@@ -169,7 +169,7 @@ class Directory_Services:
         Example:
             - Directory_Services.remove_ldap()
         """
-        if COM.is_visible(xpaths.common_xpaths.any_text('Active Directory and LDAP are disabled.')):
+        if COM.is_visible(xpaths.common_xpaths.any_text('Active Directory and LDAP are disabled.')) is False:
             if not COM.is_visible(xpaths.common_xpaths.any_text('To enable disable Active Directory first.')):
                 if COM.is_visible(xpaths.common_xpaths.button_field('configure-ldap')):
                     cls.click_configure_ldap_button()
@@ -182,5 +182,9 @@ class Directory_Services:
                 COM.clear_input_field('bindpw')
                 COM.unset_checkbox('enable')
                 COM.click_save_button()
+                if COM.is_visible(xpaths.common_xpaths.any_text('Error: has_samba_schema')):
+                    print("Error: has_samba_schem encountered. NAS-128958")
+                    COM.click_error_dialog_close_button()
+                    COM.close_right_panel()
         assert COM.is_card_not_visible('LDAP') is True
         assert cls.is_ldap_configure_button_visible() is True
