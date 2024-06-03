8f483cfe5a380f525a272ff9b9878f8e6efbf2f4
ericbsd <ericturgeon.bsd@gmail.com>
2024-06-03 14:58:01 -0400

    Refactored all BE action into assert_boot_environment_element_restricted

--- keywords/webui/boot.py
+++ keywords/webui/boot.py
@@ -24,6 +24,35 @@ class Boot:
         return result
 
     @classmethod
+    def assert_boot_environment_element_restricted(cls, be_name: str, action: str) -> bool:
+        """
+        This method returns True or False whether the link is restricted or not.
+
+        :param be_name: The name of the boot environment.
+        :param action: The name of the action link on the boot environment.
+            Options: [activate, clone, delete, keep, rename]
+        :return: True if the link is restricted and not clickable otherwise it returns False.
+
+        Example:
+            - Boot.assert_boot_environment_element_restricted('test-bootenv', 'activate')
+        """
+        be_xpath = COM.convert_to_tag_format(be_name)
+        match action:
+            case('activate'):
+                action_xpath = 'mdi-check-decagram'
+            case('clone'):
+                action_xpath = 'mdi-content-copy'
+            case('delete'):
+                action_xpath = 'mdi-delete'
+            case('keep'):
+                action_xpath = 'bookmark'
+            case('rename'):
+                action_xpath = 'mdi-rename-box'
+            case _:
+                assert False, f'Invalid action: {action}'
+        return COM.assert_button_is_restricted(f'bootenv-{be_xpath}-{action_xpath}-row-action')
+
+    @classmethod
     def assert_boot_environment_row_exist(cls, be_name: str) -> bool:
         """
         This method returns True or False whether the boot environment exists in the list.
@@ -49,73 +78,3 @@ class Boot:
             - Boot.assert_boot_pool_status('active')
         """
         return WebUI.wait_until_visible(xpaths.boot.boot_pool_status(status))
-
-    @classmethod
-    def assert_activate_boot_environment_is_restricted(cls, be_name: str) -> bool:
-        """
-        This method returns True or False whether the Activate button is locked and not clickable.
-
-        :param be_name: The name of the boot environment.
-        :return: True if the Activate button is locked and not clickable otherwise it returns False.
-
-        Example:
-            - Boot.assert_activate_boot_environment_is_restricted('test-bootenv')
-        """
-        be_xpath = COM.convert_to_tag_format(be_name)
-        return COM.assert_button_is_restricted(f'bootenv-{be_xpath}-mdi-check-decagram-row-action')
-
-    @classmethod
-    def assert_clone_boot_environment_is_restricted(cls, be_name: str) -> bool:
-        """
-        This method returns True or False whether the Clone button is locked and not clickable.
-
-        :param be_name: The name of the boot environment.
-        :return: True if the Clone button is locked and not clickable otherwise it returns False.
-
-        Example:
-            - Boot.assert_clone_boot_environment_is_restricted('test-bootenv')
-        """
-        be_xpath = COM.convert_to_tag_format(be_name)
-        return COM.assert_button_is_restricted(f'bootenv-{be_xpath}-mdi-content-copy-row-action')
-
-    @classmethod
-    def assert_delete_boot_environment_is_restricted(cls, be_name: str) -> bool:
-        """
-        This method returns True or False whether the Delete button is locked and not clickable.
-
-        :param be_name: The name of the boot environment.
-        :return: True if the Delete button is locked and not clickable otherwise it returns False.
-
-        Example:
-            - Boot.assert_delete_boot_environment_is_restricted('test-bootenv')
-        """
-        be_xpath = COM.convert_to_tag_format(be_name)
-        return COM.assert_button_is_restricted(f'bootenv-{be_xpath}-mdi-delete-row-action')
-
-    @classmethod
-    def assert_keep_boot_environment_is_restricted(cls, be_name: str) -> bool:
-        """
-        This method returns True or False whether the Keep button is locked and not clickable.
-
-        :param be_name: The name of the boot environment.
-        :return: True if the Keep button is locked and not clickable otherwise it returns False.
-
-        Example:
-            - Boot.assert_keep_boot_environment_is_restricted('test-bootenv')
-        """
-        be_xpath = COM.convert_to_tag_format(be_name)
-        return COM.assert_button_is_restricted(f'bootenv-{be_xpath}-bookmark-row-action')
-
-    @classmethod
-    def assert_rename_boot_environment_is_restricted(cls, be_name: str) -> bool:
-        """
-        This method returns True or False whether the Rename button is locked and not clickable.
-
-        :param be_name: The name of the boot environment.
-        :return: True if the Rename button is locked and not clickable otherwise it returns False.
-
-        Example:
-            - Boot.assert_rename_boot_environment_is_restricted('test-bootenv')
-        """
-        be_xpath = COM.convert_to_tag_format(be_name)
-        return COM.assert_button_is_restricted(f'bootenv-{be_xpath}-mdi-rename-box-row-action')
