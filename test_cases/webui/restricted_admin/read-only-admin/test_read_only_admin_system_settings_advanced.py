4176e402b4e605e19c4e53b47aab1421a5a69cdb
markbainix <mark.bain@ixsystems.com>
2024-06-03 12:26:57 -0400

    refactor is_visible for each object

--- test_cases/webui/restricted_admin/read-only-admin/test_read_only_admin_system_settings_advanced.py
+++ test_cases/webui/restricted_admin/read-only-admin/test_read_only_admin_system_settings_advanced.py
@@ -30,100 +30,100 @@ class Test_Read_Only_Admin_System_Settings_Advanced:
         2. Verify the read-only admin is able to see various Card details (Console, Syslog, Audit, Kernel)
         """
         assert COM.assert_page_header('Advanced')
-        assert COM.is_visible(xpaths.common_xpaths.button_field('save-debug')) is True
+        assert COM.is_button_visible('save-debug') is True
 
         # Console Card
         assert COM.is_card_visible('Console') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('console-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Enable Serial Console:')) is True
+        assert COM.is_button_visible('console-configure') is True
+        assert COM.is_text_visible('Enable Serial Console:') is True
 
         # Syslog Card
         assert COM.is_card_visible('Syslog') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('syslog-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Syslog Level:')) is True
+        assert COM.is_button_visible('syslog-configure') is True
+        assert COM.is_text_visible('Syslog Level:') is True
 
         # Audit Card
         assert COM.is_card_visible('Audit') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('audit-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Reservation:')) is True
+        assert COM.is_button_visible('audit-configure') is True
+        assert COM.is_text_visible('Reservation:') is True
 
         # Kernel Card
         assert COM.is_card_visible('Kernel') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('kernel-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Enable Debug Kernel:')) is True
+        assert COM.is_button_visible('kernel-configure') is True
+        assert COM.is_text_visible('Enable Debug Kernel:') is True
 
         # Cron Jobs Card
         assert COM.is_card_visible('Cron Jobs') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('cron-add')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Next Run')) is True
+        assert COM.is_button_visible('cron-add') is True
+        assert COM.is_text_visible('Next Run') is True
         # Cron Jobs page
         COM.click_link('cron-jobs-open-in-new')
         assert COM.assert_page_header('Cron Jobs') is True
-        assert COM.is_visible(xpaths.common_xpaths.input_field('search')) is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('columns')) is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('add-cronjob')) is True
-        assert COM.is_visible(xpaths.common_xpaths.select_field('page-size')) is True
+        assert COM.is_input_visible('search') is True
+        assert COM.is_button_visible('columns') is True
+        assert COM.is_button_visible('add-cronjob') is True
+        assert COM.is_select_visible('page-size') is True
         NAV.navigate_to_system_settings_advanced()
 
         # Init/Shutdown Scripts Card
         assert COM.is_card_visible('Init/Shutdown Scripts') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('init-shutdown-add')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Timeout')) is True
+        assert COM.is_button_visible('init-shutdown-add') is True
+        assert COM.is_text_visible('Timeout') is True
         # Init/Shutdown Scripts page
         COM.click_link('init-shutdown-open-in-new')
         assert COM.assert_page_header('Init/Shutdown Scripts') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('add-init-shutdown-script')) is True
-        assert COM.is_visible(xpaths.common_xpaths.select_field('page-size')) is True
+        assert COM.is_button_visible('add-init-shutdown-script') is True
+        assert COM.is_select_visible('page-size') is True
         NAV.navigate_to_system_settings_advanced()
 
         # Sysctl Card
         assert COM.is_card_visible('Sysctl') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('sysctl-add')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Var')) is True
+        assert COM.is_button_visible('sysctl-add') is True
+        assert COM.is_text_visible('Var') is True
         # Sysctl page
         COM.click_link('sysctl-open-in-new')
         assert COM.assert_page_header('Sysctl') is True
-        assert COM.is_visible(xpaths.common_xpaths.input_field('search')) is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('add-tunable')) is True
-        assert COM.is_visible(xpaths.common_xpaths.select_field('page-size')) is True
+        assert COM.is_input_visible('search') is True
+        assert COM.is_button_visible('add-tunable') is True
+        assert COM.is_select_visible('page-size') is True
         NAV.navigate_to_system_settings_advanced()
 
         # Storage Card
         assert COM.is_card_visible('Storage') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('storage-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('System Dataset Pool:')) is True
+        assert COM.is_button_visible('storage-configure') is True
+        assert COM.is_text_visible('System Dataset Pool:') is True
 
         # Replication Card
         assert COM.is_card_visible('Replication') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('replication-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Replication Tasks Limit:')) is True
+        assert COM.is_button_visible('replication-configure') is True
+        assert COM.is_text_visible('Replication Tasks Limit:') is True
 
         # Access Card
         assert COM.is_card_visible('Access') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('sessions-terminate')) is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('sessions-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Token Lifetime:')) is True
+        assert COM.is_button_visible('sessions-terminate') is True
+        assert COM.is_button_visible('sessions-configure') is True
+        assert COM.is_text_visible('Token Lifetime:') is True
         assert COM.is_visible(xpaths.system.advanced_access_terminate_session()) is True
 
         # Allowed IP Addresses Card
         assert COM.is_card_visible('Allowed IP Addresses') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('configure-ip-address')) is True
+        assert COM.is_button_visible('configure-ip-address') is True
 
         # TODO: Uncomment when NAS-129329 fixed
         # # Self-Encrypting Drive Card
         # assert COM.is_card_visible('Self-Encrypting Drive') is True
-        # assert COM.is_visible(xpaths.common_xpaths.button_field('self-encrypted-drive-configure')) is True
-        # assert COM.is_visible(xpaths.common_xpaths.any_text('ATA Security User:')) is True
+        # assert COM.is_button_visible('self-encrypted-drive-configure') is True
+        # assert COM.is_text_visible('ATA Security User:') is True
 
         # Isolated GPU Device(s) Card
         assert COM.is_card_visible('Isolated GPU Device(s)') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('isolated-gpus-devices-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('No Isolated GPU Device(s) configured')) is True
+        assert COM.is_button_visible('isolated-gpus-devices-configure') is True
+        assert COM.is_text_visible('No Isolated GPU Device(s) configured') is True
 
-        # Global Two Factor Authentication Card
+        # Global Two-Factor Authentication Card
         assert COM.is_card_visible('Global Two Factor Authentication') is True
-        assert COM.is_visible(xpaths.common_xpaths.button_field('two-factor-auth-configure')) is True
-        assert COM.is_visible(xpaths.common_xpaths.any_text('Global 2FA:')) is True
+        assert COM.is_button_visible('two-factor-auth-configure') is True
+        assert COM.is_text_visible('Global 2FA:') is True
 
     @allure.tag("Update")
     @allure.issue('NAS-129333', 'NAS-129333')
